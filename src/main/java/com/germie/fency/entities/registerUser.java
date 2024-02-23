32bdf101dea87ca811e403970e5988b3c522728c
BSKYB\mjf442 <fencyj854@gmail.com>
2024-02-23 14:48:49 +0530

    reg2

--- src/main/java/com/germie/fency/entities/registerUser.java
+++ src/main/java/com/germie/fency/entities/registerUser.java
@@ -1,20 +1,24 @@
 package com.germie.fency.entities;
+import org.springframework.security.crypto.bcrypt.*;
+
+import jakarta.annotation.Nonnull;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class registerUser {
-
+    
+    
     public String uname;
     public long mobile;
     public String email;
     public String password;
 
     public registerUser(String uname, long mobile, String email, String password){
-       
+         BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder(12);
          this.uname=uname;
          this.email=email;
          this.mobile=mobile;
-         this.password=password;
+         this.password=bcrypt.encode(password);
     }
 }
